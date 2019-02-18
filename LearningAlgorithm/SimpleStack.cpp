012ff21b654aa6d8b8fb2155cf0fd5d5db71b464
DrMondor <826249611@qq.com>
2019-02-18 20:48:44 +0800

    .

--- LearningAlgorithm/SimpleStack.cpp
+++ LearningAlgorithm/SimpleStack.cpp
@@ -6,11 +6,15 @@
 
 #include "stdafx.h"
 #include "SimpleStack.h"
+#include <iostream>
 struct Node;
 typedef struct Node *PtrToNode;
 typedef PtrToNode Stack;
 
 int IsEmpty(Stack S);
+Stack CreatStack(void);
+void MakeEmpty(Stack S);
+void Pop(Stack S);
 
 struct  Node
 {
@@ -20,4 +24,28 @@ struct  Node
 
 int IsEmpty(Stack S) {
 	return S  -> Next == NULL;
+}
+Stack CreatStack(void) {
+	Stack S;
+
+	S = (Stack)malloc(sizeof(struct Node));
+	if (S == NULL)
+		printf("Out of space");
+	S->Next == NULL;
+	MakeEmpty(S);
+	return S;
+}
+void MakeEmpty(Stack S) {
+
+	if (S == NULL)
+	{
+		printf("Must use CreateStack first");
+	}
+	else
+	{
+		while (!IsEmpty(S))
+		{
+			Pop(S);
+		}
+	}
 }
