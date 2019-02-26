29b1be530e08bba1e824ab765b1ddb618f44b0ad
DrMondor <826249611@qq.com>
2019-02-26 22:29:30 +0800

    add

--- LearningAlgorithm/PostfixExpressionTransfer.cpp
+++ LearningAlgorithm/PostfixExpressionTransfer.cpp
@@ -1,3 +1,9 @@
+/*-------------------------------------
+一次栈的实践
+         -简单计算器
+--------------------------------------*/
+
+
 #include "stdafx.h"
 #include "PostfixExpressionTransfer.h"
 #include <iostream>
@@ -8,8 +14,24 @@ void InputData();
 
 void InputData() {
 
-	char name[10];
+	int stack[10];
+	char signal[5];
+	char data[20];
 	cout << "请输入算式\n";
-	cin.getline(name, 10);
+	cin.getline(data, 20);
+
+	for (int i = 0; i < 20; i++) {
+
+
+		if (data[i]<9 && data[i] > 0)
+		{
+			
+		}
+
+
+	}
+	cout << data[3];
+
+	cin >> data;
 
 }
