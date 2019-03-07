76c426d58b248fb1f3437d70a45cbb2dda94ca51
DrMondor <826249611@qq.com>
2019-03-07 15:30:42 +0800

    add

---
+++ LearningAlgorithm/Tree.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "stdafx.h"
+
+typedef struct TreeNode *ptrToNode;
+typedef struct TreeBinaryNode *ptrToNodeBinary;
+typedef struct ptrToNodeBinary Tree;
+
+
+struct TreeNode
+{
+	int Element;
+	ptrToNode FirstChild; //第一孩子结点
+	ptrToNode NextSibling; //旁兄弟结点
+};
+
+struct TreeBinaryNode
+{
+	int Element;
+	ptrToNodeBinary Left;
+	Tree right;
+};
